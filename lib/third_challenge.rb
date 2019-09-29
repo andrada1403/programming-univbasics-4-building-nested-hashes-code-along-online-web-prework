4610ddf057b9895816ca355801871ece743a0062
Andrada Lorena Popa <andrada.lorena.popa@gmail.com>
2019-09-29 19:39:34 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -10,7 +10,7 @@ def third_challenge
       :patriarch => {:name=>"Lord Capulet",:age=>"50"
         #your key/value pairs here
         },
-      :matriarch => {:name=>"Lady Capulet",:age=>"50"
+      :matriarch => {:name=>"Lady Capulet",:age=>"51"
         #your key/value pairs here
         },
       :heroine => {:name=>"Juliet",:age=>"15",:status=>"alive"
